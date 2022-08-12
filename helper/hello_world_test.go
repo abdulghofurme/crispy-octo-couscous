ea9722032a2f1d7c95a3a71c897143e4071ef261
Abdul Ghofur <abdulghofur.me@gmail.com>
2022-08-12 15:32:38 +0700

    Menggagalkan Test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Abdul")
-	if result != "Hello Abdul" {
-		panic("Result is not 'Hello Abdul")
+	if result != "Hi Abdul" {
+		t.Fatal("Result is not 'Hello Abdul")
 	}
 }
