c360434d8e3af6f7e4f6f83f7e39cd1a8f903d78
Abdul Ghofur <abdulghofur.me@gmail.com>
2022-08-12 16:10:13 +0700

    Skip Test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,6 +1,7 @@
 package helper
 
 import (
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,4 +10,12 @@ import (
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Abdul")
 	assert.Equal(t, "Hello Abdul", result, "Result is not 'Hello Abdul")
+}
+
+func TestSkipHelloWorld(t *testing.T) {
+	if runtime.GOOS == "darwin" {
+		t.Skip("Can not run on Mac OS")
+	}
+	result := HelloWorld("OS")
+	assert.Equal(t, "Hello OS", result, "Result is not 'Hello OS")
 }
